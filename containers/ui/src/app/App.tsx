4fa9f27d9941038ea2b28337ed4675338efd4eb8
GeorgeMSchaffer <gschaffer@gmail.com>
2020-08-25 10:32:06 -0700

    Adds react router skeleton

--- containers/ui/src/app/App.tsx
+++ containers/ui/src/app/App.tsx
@@ -1,19 +1,19 @@
 import React,{useEffect,useContext,useState,MouseEvent} from 'react';
-import { ApolloProvider,gql,ApolloClient, InMemoryCache,useQuery  } from '@apollo/client';
-import '../App.css';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import TodoList from '../features/todoList/TodoList';
-
 import { MuiThemeProvider, createMuiTheme,makeStyles,useTheme ,createStyles} from '@material-ui/core/styles';
-import { Theme, Container,Button ,AppBar, Menu, MenuItem,Drawer, Toolbar,Snackbar,Typography,Divider,List,ListItemIcon,ListItemText,CssBaseline,Paper } from "@material-ui/core";
+import { Theme,Grid,Box, Container,Button ,AppBar, Menu, MenuItem,Drawer, Toolbar,Snackbar,Typography,Divider,List,ListItemIcon,ListItemText,CssBaseline,Paper } from "@material-ui/core";
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import clsx from 'clsx';
 import Sidebar from '../views/Sidebar';
 import Header from '../views/Header';
-import { Provider } from 'react-redux'
-import * as serviceWorker from '../serviceWorker';
-import store from './store'
+import { Provider } from 'react-redux';
+import store from './store';
 import AddTodo from '../features/todoList/AddTodo';
+import AutoGrid from '../demos/AutoGrid';
+import '../App.css';
+import NestedGridDemo from '../demos/NestedGrid';
 
 const width = 240;
 const useStyles = makeStyles((theme: Theme) =>
@@ -62,11 +62,15 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     container: {
       display: 'flex',
+      border: '1px dashed grey',
       flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
       height: `100%`,
     },
+    box: {
+      border:'1px solid black'
+    }
   }),
 );
 
@@ -89,78 +93,91 @@ const data = [{
 
 function App() {
   const classes = useStyles();
-  console.debug('\r\n ---- CLASSES ------- ',classes);
-  const theme:Theme = useTheme();
-  console.debug('\r\n ------ THEME ------',theme);
+  console.debug('\r\n ---- CLASSES ------- ', classes);
+  const theme: Theme = useTheme();
+  console.debug('\r\n ------ THEME ------', theme);
   const [open, setOpen] = React.useState(true);
-  const handleDrawerOpen = (evt:MouseEvent)=>{
+  const handleDrawerOpen = (evt: MouseEvent) => {
     setOpen(!open);
   };
 
   return (
-    <MuiThemeProvider theme={theme}>
-    <Paper className={classes.paper}>
-    <div className={classes.container}>
-          <header>
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerOpen}
-                edge="start"
-                className={clsx(classes.menuButton, open && classes.hide)}
-              >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Persistent drawer
-          </Typography>
-        </Toolbar>
-          </header>
-          <Sidebar open={open} width={width}> SIDE BAR STUFF HERE</Sidebar>
-            <h1 className="header">
-              <AddTodo/>
-              <TodoList/>
-            </h1>
-            </div>
-        </Paper>
-      </MuiThemeProvider>
-  );
+		<Router>
+      <MuiThemeProvider theme={theme}>
+        <Container maxWidth="sm" className={classes.container}>
+          <h2>Container</h2>
+          <Box my={4} className={classes.box}>
+            <h2>Box</h2>
+							<header>
+								<Toolbar>
+									<IconButton
+										color="inherit"
+										aria-label="open drawer"
+										onClick={handleDrawerOpen}
+										edge="start"
+										className={clsx(classes.menuButton, open && classes.hide)}>
+										<MenuIcon />
+									</IconButton>
+									<Typography variant="h6" noWrap>
+										Persistent drawer
+									</Typography>
+								</Toolbar>
+							</header>
+							<Sidebar open={open} width={width}>
+								{' '}
+								SIDE BAR STUFF HERE
+							</Sidebar>
+							<h1 className="header">
+								<AddTodo />
+								<TodoList />
+							</h1>
+					</Box>
+				</Container>
+			</MuiThemeProvider>
+			<Route path="/autogrid" exact component={AutoGrid} />
+			<Route path="/muigrid" component={NestedGridDemo} />
+		</Router>
+	);
 }
-// later...
+
+  // later...
 export default App;
 
-//#region Greeter
-
-interface gretterState {}
-interface gretterProps {
-  fname: string,
-  lname: string,
-  age?: number
-}
-/*
-Interface Example
-
-However, many hooks are initialized with null-ish default values, and you may wonder how to provide types. 
-Explicitly declare the type, and use a union type:
+/* 
+4  function Index() {
+5    return <h2>Home</h2>;
+6  }
+7 
+8  function Product({ match }) {
+9   return <h2>This is a page for product with ID: {match.params.id} </h2>;
+10 }
+11 
+12 function AppRouter() {
+13   return (
+14     <Router>
+15       <div>
+16         <nav>
+17           <ul>
+18             <li>
+19               <Link to="/">Home</Link>
+20             </li>
+21             <li>
+22               <Link to="/products/1">First Product</Link>
+23             </li>
+24             <li>
+25               <Link to="/products/2">Second Product</Link>
+26             </li>
+27           </ul>
+28         </nav>
+29 
+30         <Route path="/" exact component={Index} />
+31         <Route path="/products/:id" component={Product} />
+32       </div>
+33     </Router>
+34   );
+35 }
+36 
 */
-interface IUser {
-  fname: string,
-  lname: string,
-  age?: number //optinal parameter
-}
-
-function Greet(props: gretterProps){
-  const {fname,lname,age} = props;
-  return(
-    <div>
-     <div>First Name: {fname}</div>
-     <div>Last Name: {lname}</div>
-    </div>
-  )
-}
-
-
  //#endregion
 
 /*
